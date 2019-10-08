3b853e26e89fbec9fd94d09d164bbf5785ed20cc
kevin6767 <48367743+kevin6767@users.noreply.github.com>
2019-10-07 20:00:39 -0500

    recommitting to repository

---
+++ README.md
@@ -0,0 +1,10 @@
+# twitbot
+Twitter bot for sending tweets
+
+UPDATE 10/3
+Creating a twitter bot using Node.js and a couple of libaries. Currently the 1.0 version only favorites tweets. But soon I want it to be able to
+direct message people and engage in full on conversations using a database of responses. 
+
+10/7 
+
+Bot now replies and sends tweets out. I have added a small array of responses for each of them. 
---
+++ eslickBot.js
@@ -0,0 +1,67 @@
+console.log('It is starting');
+var Twit = require('twit');
+var message = require('./responses/messages.js');
+var insults = require ('./responses/insults.js');
+
+require('dotenv').config();
+
+const T = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+
+
+
+  
+
+var stream = T.stream('statuses/filter', { track: '@BotEslick' });
+
+stream.on('tweet', tweetEvent);
+
+
+function tweetEvent(tweet) {
+    console.log('About to reply...')
+    var name = tweet.user.screen_name;
+    var nameID  = tweet.id_str;
+    var insult = insults[Math.floor(Math.random()*insults.length)];
+    var reply = "You mentioned me! @" + name + ' - ' + insult;
+    var params             = {
+                              status: reply,
+                              in_reply_to_status_id: nameID
+                             };
+
+    T.post('statuses/update', params, function(err, _data, _response) {
+      if (err !== undefined) {
+        console.log(err);
+      } else {
+        console.log('Tweeted: ' + params.status);
+      }
+    })
+};
+
+tweetIt();
+
+
+function tweetIt() { // Function to send tweets
+  console.log('Starting...');
+  var randomItem = message[Math.floor(Math.random()*message.length)];
+  var txt = randomItem;
+  var params             = {
+                      status: txt,
+      
+      };
+      console.log('About to post...');
+    T.post('statuses/update', params, function(err, _data, _response) {
+    if (err !== undefined) {
+    console.log(err);
+    } else {
+    console.log('Tweeted: ' + params.status);
+    }
+    })
+};
+
+
+
